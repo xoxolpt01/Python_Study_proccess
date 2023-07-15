20b4281830d21812f92c538e5e69c9999fe0f14c
Alexander <55795742+xoxolpt01@users.noreply.github.com>
2023-07-16 00:35:00 +0100

    Add files via upload

---
+++ TkInter/NumPy.py
@@ -0,0 +1,11 @@
+from tkinter import *
+# def summ():
+#     l = Label(root, text="Хуй", fg="black").place(x = 200, y = 500)
+#
+# root = Tk()
+# root.geometry("1280x720")
+# b = Button(root, width=20, height=5, text="GO", command=summ)
+# b.place(x= 100, y = 100)
+# root.mainloop()
+
+print(max(1,2,a))
